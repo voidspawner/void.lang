1bd23de75ef0bed580d323823900fb9bcfe9d7c2
R <r@void.earth>
2023-07-16 23:34:56 +0500

    init directories and files

---
+++ java/Void.java
@@ -0,0 +1,12 @@
+/*
+ * V O I D spawner lang java
+ * void.lang.java
+ */
+
+public class Void {
+
+	public static void main(String[] args) {
+		
+	}
+
+}
---
+++ python/void.py
@@ -0,0 +1,11 @@
+#
+# V O I D spawner lang python
+# void.lang.python
+#
+
+class Void:
+	
+	pass
+
+if __name__ == "__main__":
+	void = Void()
